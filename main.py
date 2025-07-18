27aa6a7ac25c32ef8c97e31a069b2d03a1136f31
DarrenRickard <drickard94@gmail.com>
2025-07-19 02:06:38 +0900

    added player object to game window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,8 +8,13 @@ def main():
     fpsClock = pygame.time.Clock()
     dt = 0 
     fps = 60
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    # Initialize the player
+    player = Player(x, y)
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = fpsClock.tick(fps) / 1000
         for event in pygame.event.get():
