afc0efa3b634921d44df9fc3b76cb81626f3f3de
DarrenRickard <drickard94@gmail.com>
2025-07-19 01:22:38 +0900

    implemented fps clock

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fpsClock = pygame.time.Clock()
+    dt = 0 
+    fps = 60
     while True:
         screen.fill("black")
         pygame.display.flip()
+        fpsClock.tick(fps) 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
 
 
 if __name__ == "__main__":
