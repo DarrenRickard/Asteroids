91321208411a9d7fc3e510cb7822b30d63fbe071
DarrenRickard <drickard94@gmail.com>
2025-07-19 02:52:22 +0900

    added updateable and drawable groups

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def main():
     fps = 60
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     # Initialize the player
     player = Player(x, y)
     while True:
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        # Update and draw all sprites
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        # Update display
         pygame.display.flip()
         dt = fpsClock.tick(fps) / 1000
         for event in pygame.event.get():
